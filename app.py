8b1ba23806ebb32fffb6f2071765c63ac7360937
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-02-05 00:49:10 +0200

    Added Flask-PyMongo

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# app entry point
+from flask import Flask
+from flask import render_template
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+app.config.from_pyfile('flask-config.cfg')
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/post/<int:id>')
+def get_post(id):
+    return render_template('post.html')
+
+def all_posts():
+    return mongo.db.posts.find()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
