612a6f7470418d83b1df5ccf909a84333ca731a9
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-02-05 02:57:22 +0200

    using variable port number

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def all_posts():
     return mongo.db.posts.find()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
