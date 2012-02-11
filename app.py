93b52571a986cbba2506766bf6f92de7b475ee60
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-02-11 10:05:58 +0200

    using MongoEngine as ORM, removed venv from source tree

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template
-from flask.ext.pymongo import PyMongo
-import os, bcrypt
+from mongoengine import *
+from datetime import datetime
+import os, bcrypt, config
 
+# Initialization:
+###################################################################
 app = Flask(__name__)
-app.config.from_pyfile('flask-config.cfg')
-mongo = PyMongo(app)
+connect(config.DBNAME, host=config.HOST, port=config.PORT, \
+        username=config.USERNAME, password=config.PASSWORD)
 
+# Models:
+###################################################################
+class Post(Document):
+    title = StringField(required=True)
+    content = StringField(required=True)
+    published_at = datetime.utcnow()
+    ID = IntField(min_value=1)
+
+def create_post(title, content):
+    Post(title=title, content=content, ID=Post.objects.count() + 1).save()
+
+def all_posts():
+    return Post.objects
+
+# VIEWS:
+####################################################################
 @app.route('/')
 def index():
-    return render_template('index.html', posts=all_posts(), header="Programming Beans") 
+    return render_template('index.html', posts=all_posts()) 
 
 @app.route('/post/<int:id>')
 def get_post(id):
@@ -18,13 +37,12 @@ def get_post(id):
 @app.route('/new', methods=['POST', 'GET'])
 def post_new():
     if request.method == 'POST':
-        return 'Title: %s' % request.form['title'] + '\n' + \
-        'Body: %s' % request.form['body']
+        title = request.form['title']
+        content = request.form['body']
+        create_post(title, content)
+        return "Post created."
     else:
         return render_template('new.html')
-
-def all_posts():
-    return mongo.db.posts.find()
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -38,12 +56,15 @@ def login():
     else:
         return render_template('login.html')
 
+################################################################################
+
 def validate(user, password):
     hashed = '$2a$12$leF7cVimpflh2P97U4WV8ugjE9HE5fwyeb5shGXjVvFuJoxSj08cC'
     return bcrypt.hashpw(password, hashed) == hashed and user == 'mhh91'
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     # Disable this when deploying
-    #app.debug = True
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
