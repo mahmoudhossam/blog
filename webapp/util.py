90bbf47f3772400da3f5cf59e83bda0bf11545f4
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-02-20 22:56:06 +0200

    changed post url

--- webapp/util.py
+++ webapp/util.py
@@ -1,13 +1,29 @@
 import bcrypt
-from models import Post
+from models import Post, User
+from slugify import slugify
+from datetime import datetime
 
 def validate(user, password):
     hashed = '$2a$12$leF7cVimpflh2P97U4WV8ugjE9HE5fwyeb5shGXjVvFuJoxSj08cC'
     return bcrypt.hashpw(password, hashed) == hashed and user == 'mhh91'
 
-def create_post(title, content):
-    Post(title=title, content=content, ID=Post.objects.count() + 1).save()
+def login(user, password):
+    results = Post.objects(username=user)
+    if len(results) != 0:
+        user = results.first()
+        return bcrypt.hashpw(password, user.hashed_pw) == user.hashed_pw
+
+def create_post(title, content, published_at=datetime.today()):
+    slug = slugify(title)
+    post = Post(title=title, content=content, \
+            published_at=published_at, slug=slug)
+    post.save()
 
 def all_posts():
     return Post.objects
+
+def get_post(year, month, day, slug):
+    date = datetime(year, month, day)
+    post = Posts.objects(published_at=date, slug=slug)
+    return post.first()
 
