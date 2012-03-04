dbae8c635fdecd244d2d42a0918781d8ca90ad8f
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-03-05 01:56:22 +0200

    fixed connection parameters

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,7 @@ from config import login
 from mongoengine import connect
 
 app = Flask(__name__)
-connect(login['db'], host=login['host'], port=login['port'], \
-        username=login['user'], password=login['password'])
+connect(login.db, host=login.host, port=login.port, \
+        username=login.user, password=login.password)
 
 import webapp.views, webapp.models
